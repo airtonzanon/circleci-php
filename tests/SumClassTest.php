86440772c8449c432229e0f9aa6db14f6fc7e156
Airton Zanon <me@airton.dev>
2019-05-20 18:22:16 +0200

    Run phpcs also for tests
    Signed-off-by: Airton Zanon <me@airton.dev>

--- tests/SumClassTest.php
+++ tests/SumClassTest.php
@@ -1,32 +1,32 @@
 <?php
 
+namespace CircleCIPHP\Test;
+
 use PHPUnit\Framework\TestCase;
 use CircleCIPHP\SumClass;
 
 class TestSumClass extends \PHPUnit_Framework_TestCase
 {
 
-  private $class;
+    private $class;
 
-  public function setUp()
-  {
-    $this->class = new SumClass();
-  }
+    public function setUp()
+    {
+        $this->class = new SumClass();
+    }
 
-  public function testShouldSumNumbers()
-  {
-    $result = $this->class->method(5, 5);
+    public function testShouldSumNumbers()
+    {
+        $result = $this->class->method(5, 5);
 
-    $this->assertEquals(10, $result);
-  }
+        $this->assertEquals(10, $result);
+    }
 
   /**
   * @expectedException \Error
   */
-  public function testShouldNotAcceptStringToSum()
-  {
-    $this->class->method(5, '');
-  }
-
+    public function testShouldNotAcceptStringToSum()
+    {
+        $this->class->method(5, '');
+    }
 }
-
